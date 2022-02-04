62eba4b2b1cc5f1417db4fff5655c60d2978186d
swathiperla <98946392+perlaritgithub@users.noreply.github.com>
2022-02-04 16:16:21 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,57 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+    footer.innerHTML = `
+    <div class="footer-content">
+    <img src="img/light-logo.png"  class="logo" alt="">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+        <ul class="category">
+            <li class="category-title">women</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+    </div>
+   
+</div>
+<p class="footer-title">about  company</p>
+<p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, repellat consectetur? Aperiam dolorem quidem, modi nesciunt eligendi id? Exercitationem obcaecati vel error sapiente illum laborum temporibus quis id similique, maxime, nesciunt doloremque non facilis at nisi magni voluptate accusantium dolores itaque? Deleniti quisquam blanditiis qui non dolorum in facere ex, quaerat, saepe tempora cupiditate! Consectetur ad iure, accusantium magnam ullam vero pariatur maiores error est ab magni expedita eveniet reprehenderit perferendis necessitatibus architecto. Repudiandae reiciendis architecto facere culpa quidem voluptate debitis, corporis velit numquam nobis, nesciunt sed nam, natus magnam distinctio non labore vel laudantium voluptates quo tempora ex necessitatibus.
+    
+</p>
+<p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+<p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+<div class="footer-social-container">
+    <div>
+        <a href="#" class="social-link">terms & services</a>
+        <a href="#" class="social-link">privacy page</a>
+    </div>
+    <div>
+        <a href="#" class="social-link">instagram</a>
+        <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+    </div>
+</div>
+<p class="footer-credit">Clothing, Best apparels Online Store</p>
+    
+    `;
+}
+createFooter();
