5e149a5b70ebf435ad66c588ed7b02a08c1cba83
swathiperla <98946392+perlaritgithub@users.noreply.github.com>
2022-02-09 12:35:23 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ const createFooter = () => {
     let footer = document.querySelector('footer');
     footer.innerHTML = `
     <div class="footer-content">
-    <img src="img/light-logo.png"  class="logo" alt="">
+    <img src="./light-logo.png"  class="logo" alt="">
     <div class="footer-ul-container">
         <ul class="category">
             <li class="category-title">men</li>
