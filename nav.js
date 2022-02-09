f7458550eb08ba6285860584c6a0c65aec2f2023
swathiperla <98946392+perlaritgithub@users.noreply.github.com>
2022-02-09 12:33:12 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,14 +2,14 @@ const createNav = () => {
     let nav = document.querySelector('.navbar');
     nav.innerHTML = `
     <div class="nav">
-    <img src="img/dark-logo.png" class="brand-logo" alt="">
+    <img src="./dark-logo.png" class="brand-logo" alt="">
     <div class="nav-items">
         <div class="search">
             <input type="text" class="search-box" placeholder="search brand, product">
             <button class="search-btn">search</button>
         </div>
-        <a href="#"><img src="img/user.png" alt=""></a>
-        <a href="#"><img src="img/cart.png" alt=""></a>
+        <a href="#"><img src="./user.png" alt=""></a>
+        <a href="#"><img src="./cart.png" alt=""></a>
     </div>
 </div>
 <ul class="links-container">
